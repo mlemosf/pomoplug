32d73e6aaebdb89d932e5d32469fe0df610f2029
Matheus Fernandes <matheuslemosf@protonmail.com>
2025-12-11 20:27:57 -0300

    refactor: Uso de View
    Dada a necessidade de implementar novos métodos, optei por usar View ao invés de TemplateView.

--- tomacheck/timer/views.py
+++ tomacheck/timer/views.py
@@ -2,7 +2,7 @@ from http.client import NOT_FOUND
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import RedirectView, TemplateView
+from django.views.generic import RedirectView, TemplateView, View
 
 from timer.models import Timer
 
@@ -17,21 +17,21 @@ class TimerCreateView(RedirectView):
         title = self.request.GET.get("t", None)
         try:
             timer = Timer.objects.get(title=title)
+            kwargs["uuid"] = timer.id
+            return super().get_redirect_url(*args, **kwargs)
         except ObjectDoesNotExist:
             timer = Timer.objects.create(title=title)
-        finally:
             kwargs["uuid"] = timer.id
             return super().get_redirect_url(*args, **kwargs)
 
 
-class TimerView(TemplateView):
-    template_name = "timer.html"
-
-    def get_context_data(self, **kwargs):
+class TimerView(View):
+    def get(self, request, *args, **kwargs):
         timer_uuid = kwargs.get("uuid")
-        print(timer_uuid)
         timer = Timer.objects.get(id=timer_uuid)
-        context = super().get_context_data(**kwargs)
-        context["title"] = timer.title
-        context["count"] = timer.timer_count
-        return context
+        context = {"title": timer.title, "count": timer.timer_count}
+        return render(request, template_name="timer.html", context=context)
+
+    def put(self, request, *args, **kwargs):
+        # TODO: Update values in the timer
+        pass
