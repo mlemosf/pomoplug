8d643a2e114328822ea9fea1a0d0a67f324624da
Matheus Fernandes <matheuslemosf@protonmail.com>
2025-12-11 19:10:08 -0300

    feat: Interface do timer

--- tomacheck/timer/views.py
+++ tomacheck/timer/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from timer.models import Timer
+
 
 # Create your views here.
 class TimerView(TemplateView):
     template_name = "timer.html"
+
+    def get_context_data(self, **kwargs):
+        timer_uuid = kwargs.get("uuid")
+        print(timer_uuid)
+        timer = Timer.objects.get(id=timer_uuid)
+        context = super().get_context_data(**kwargs)
+        context["title"] = timer.title
+        return context
