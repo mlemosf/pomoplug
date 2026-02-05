f9519e7c42fa30749c34de7cfe17137261ebb9d7
Matheus Fernandes <matheuslemosf@protonmail.com>
2026-02-04 22:30:22 -0300

    feat: Campos de timestamp na model Timer

--- pomoplug/timer/models.py
+++ pomoplug/timer/models.py
@@ -20,6 +20,8 @@ class Timer(models.Model):
         related_name="timers",
         on_delete=models.CASCADE,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return str(self.title)
