5214cd4c1a9886d936a5059030b676c92d4545e7
Matheus Fernandes <matheuslemosf@protonmail.com>
2026-02-17 20:09:19 -0300

    fix: Redirect para signin

--- pomoplug/config/settings.py
+++ pomoplug/config/settings.py
@@ -46,6 +46,8 @@ if not GOOGLE_OAUTH_CLIENT_ID:
     raise ValueError(
         "GOOGLE_OAUTH_CLIENT_ID is missing.Have you put it in a file at core/.env ?"
     )
+LOGIN_REDIRECT_URL = "/signin"
+LOGIN_URL = "/signin"
 
 # We need these lines below to allow the Google sign in popup to work.
 SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
